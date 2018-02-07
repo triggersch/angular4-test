6e0d73c4cb738250dd5961a8f2a1eaa1e8ccf900
gitclient <gitclient@server.com>
2018-02-07 08:53:19 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  articles : Article[];
+
+  constructor(){
+    this.articles = [
+      new Article('J2EE', 'http://javaee.org',3),
+      new Article('C++', 'http://cplusplus.com',2),
+      new Article('Jquery', 'http://www.jquery.org',1)
+    ];
+  }
+
+  addArticle(title : HTMLInputElement, link: HTMLInputElement ):boolean {
+    console.log(`ajout article: ${title.value} et un lien ${link.value}`);
+
+    this.articles.push(new Article(title.value, link.value,0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+  sortedArticles() : Article[]{
+    return this.articles.sort( (a: Article , b: Article  ) =>   b.votes - a.votes    );
+  }
 }
